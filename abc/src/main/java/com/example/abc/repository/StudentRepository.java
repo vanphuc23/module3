306960237a824a1632ce05b0de16d104b1e630b5
vanphuc23 <phucnvp15072k3@gmail.com>
2023-08-14 18:26:27 +0700

    nop

--- abc/src/main/java/com/example/abc/repository/StudentRepository.java
+++ abc/src/main/java/com/example/abc/repository/StudentRepository.java
@@ -7,9 +7,9 @@ import java.util.ArrayList;
 public class StudentRepository implements IStudentRepository{
     static ArrayList<Student> arrayList = new ArrayList<>();
     static {
-        arrayList.add(new Student("Nguyễn Văn A",8,"ĐN"));
-        arrayList.add(new Student("Nguyễn Văn B",9,"QN"));
-        arrayList.add(new Student("Nguyễn Văn C",10,"HN"));
+        arrayList.add(new Student("Nguyễn Văn A",8,"https://statics.vinpearl.com/du-lich-da-nang_1657939501.JPG"));
+        arrayList.add(new Student("Nguyễn Văn B",9,"https://statics.vinpearl.com/du-lich-da-nang_1657939501.JPG"));
+        arrayList.add(new Student("Nguyễn Văn C",10,"https://statics.vinpearl.com/du-lich-da-nang_1657939501.JPG"));
     }
     @Override
     public ArrayList<Student> findAll() {
--- apache-tomcat-9.0.21/apache-tomcat-9.0.21/webapps/ROOT/index.jsp
+++ apache-tomcat-9.0.21/apache-tomcat-9.0.21/webapps/ROOT/index.jsp
@@ -16,6 +16,10 @@
         border: 1px solid black;
         border-collapse: collapse;
     }
+    img {
+        width: 100px;
+        height: 100px;
+    }
 </style>
 <body>
 <h1>Danh sách sinh viên</h1>
@@ -31,7 +35,7 @@
             <td><c:out value="${loop.count}"/></td>
             <td><c:out value="${student.name}"/></td>
             <td><c:out value="${student.score}"/></td>
-            <td><c:out value="${student.address}"/></td>
+            <td><img src="${student.address}" alt=""></td>
         </tr>
     </c:forEach>
 </table>
